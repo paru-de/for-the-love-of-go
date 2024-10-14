20cfbf54cb79cfc46461098f17e9c1de2cb59202
paru-de <patrickr-de@outlook.de>
2024-10-14 15:06:05 +0200

    testing gofmt

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -2,6 +2,6 @@
 package calculator
 
 // Add takes two numbers and returns the result of adding them together
-func Add(a,b float64) float64 {
-	return a+b
+func Add(a, b float64) float64 {
+	return a + b
 }
