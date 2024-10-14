af7aa1935f11af08cb1f752af4289e29ea4ab512
paru-de <patrickr-de@outlook.de>
2024-10-14 15:44:18 +0200

    Add Subtract

--- calculator/calculator.go
+++ calculator/calculator.go
@@ -5,3 +5,9 @@ package calculator
 func Add(a, b float64) float64 {
 	return a + b
 }
+
+// Subtract takes two numbers a and b,
+// and returns the result of subtracting b from a.
+func Subtract(a, b float64) float64 {
+	return b - a
+}
--- calculator/calculator_test.go
+++ calculator/calculator_test.go
@@ -1,15 +1,25 @@
 package calculator_test
 
 import (
-	"calculator"
 	"testing"
+
+	"calculator"
 )
 
 func TestAdd(t *testing.T) {
 	t.Parallel()
 	var want float64 = 4
-	got := calculator.Add(2,2)
+	got := calculator.Add(2, 2)
 
+	if want != got {
+		t.Errorf("want %f, got %f", want, got)
+	}
+}
+
+func TestSubtract(t *testing.T) {
+	t.Parallel()
+	var want float64 = 2
+	got := calculator.Subtract(2, 4)
 	if want != got {
 		t.Errorf("want %f, got %f", want, got)
 	}
