3e4f16f7aa31689cfe4b26da4125df94b4229bbb
paru-de <patrickr-de@outlook.de>
2024-10-15 10:18:00 +0200

    add type Customer

--- types/main.go
+++ types/main.go
@@ -9,7 +9,14 @@ func main() {
 		Promotion       bool
 		SpecialOffer    float64
 	}
+	type Customer struct {
+		Name, FirstName, Email, Street, State, Country, City string
+		StreetNumber, ZIP                                    int
+	}
+
 	b := Book{}
+	c := Customer{}
+
 	b.Title = "All Quiet on the Western Front"
 	b.Author = "Remarque, Erich Maria"
 	b.Copies = 99
@@ -17,6 +24,18 @@ func main() {
 	b.Promotion = true
 	b.SpecialOffer = 15
 
+	c.Name = "Doe"
+	c.FirstName = "John"
+	c.Email = "booklover@mail.com"
+	c.Street = "Hauptstr."
+	c.StreetNumber = 5
+	c.State = "Baden-Württemberg"
+	c.Country = "DE"
+	c.City = "Freiburg"
+	c.ZIP = 79108
+
+	fmt.Printf("Logged in as %s. Welcome %s %s!\n\n", c.Email, c.FirstName, c.Name)
+	fmt.Printf("Shipping address:\n%s %s\n%s %d\n%d %s\n%s-%s\n\n", c.FirstName, c.Name, c.Street, c.StreetNumber, c.ZIP, c.City, c.Country, c.State)
 	fmt.Printf("%s: %s (%d. edition)\n", b.Author, b.Title, b.Edition)
 	fmt.Printf("Copies in stock: %d\n", b.Copies)
 
