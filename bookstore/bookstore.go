160c86bf1e2e292dcd2056ddfa15c4c9b2c7d711
paru-de <patrickr-de@outlook.de>
2024-10-18 09:21:42 +0200

    add Buy

--- bookstore/bookstore.go
+++ bookstore/bookstore.go
@@ -1,6 +1,17 @@
 package bookstore
 
+import "errors"
+
 type Book struct {
 	Title, Author string
 	Copies        int
+}
+
+// Buy takes type Book as input and updates the remaining Book.Copies count
+func Buy(b Book) (Book, error) {
+	if b.Copies == 0 {
+		return Book{}, errors.New("no copies left")
+	}
+	b.Copies--
+	return b, nil
 }
--- bookstore/bookstore_test.go
+++ bookstore/bookstore_test.go
@@ -14,3 +14,32 @@ func TestBook(t *testing.T) {
 		Copies: 2,
 	}
 }
+
+func TestBuy(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Title:  "Spark Joy",
+		Author: "Maria Kondo",
+		Copies: 2,
+	}
+	want := 1
+	result, err := bookstore.Buy(b)
+	if err != nil {
+		t.Fatal(err)
+	}
+	got := result.Copies
+	if want != got {
+		t.Errorf("want %d, got %d", want, got)
+	}
+}
+
+func TestBuyErrorNoCopies(t *testing.T) {
+	t.Parallel()
+	b := bookstore.Book{
+		Copies: 0,
+	}
+	_, err := bookstore.Buy(b)
+	if err == nil {
+		t.Fatal("expected error buying book with zero copies, got nil")
+	}
+}
