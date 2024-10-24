f6434eebbb090bc8f6375fc20fb84368b487a8fa
paru-de <patrickr-de@outlook.de>
2024-10-24 10:43:18 +0200

    add mybuilder

--- mytypes/mytypes.go
+++ mytypes/mytypes.go
@@ -1,5 +1,7 @@
 package mytypes
 
+import "strings"
+
 type MyInt int
 
 // Twice multiplies its receiver by 2 and returns the result
@@ -12,4 +14,24 @@ type MyStr string
 // StrLen returns the length of a string
 func (str MyStr) StrLen() int {
 	return len(str)
+}
+
+type MyBuilder struct {
+	Contents strings.Builder
+}
+
+func (mb MyBuilder) Hello() string {
+	return "Hello, Gophers!"
+}
+
+type StringUppercaser struct {
+	Contents strings.Builder
+}
+
+func (su StringUppercaser) ToUpper() string {
+	return strings.ToUpper(su.Contents.String())
+}
+
+func (input *MyInt) Double() {
+	*input *= 2
 }
